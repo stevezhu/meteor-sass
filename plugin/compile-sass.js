dedd7cdabfd6d26f1eb2901bb11508713bd6818b
Steve Zhu <st.zhu1@gmail.com>
2014-09-03 10:58:03 -0400

    renderSync -> render and support for include paths

--- plugin/compile-sass.js
+++ plugin/compile-sass.js
@@ -1,6 +1,7 @@
 var _ = Npm.require('lodash'),
 	path = Npm.require('path'),
 	fs = Npm.require('fs'),
+	Future = Npm.require('fibers/future'),
 	sass = Npm.require('node-sass'),
 	files = Npm.require('./files.js');
 
@@ -53,47 +54,113 @@ Plugin.registerSourceHandlers = function(extensions, options, handler) {
 	});
 };
 
+/**
+ * sass_options.json should only exist in either the root package dir or the root app dir.
+ */
 var OPTIONS_FILENAME = 'sass_options.json';
-Plugin.registerSourceHandler(['sass', 'scss'], {archMatching: 'web'}, function(compileStep) {
-	// return if partial
+Plugin.registerSourceHandlers(['sass', 'scss'], {archMatching: 'web'}, function(compileStep) {
+	console.log("");
+	console.log(compileStep.inputPath);
+
 	if (path.basename(compileStep.inputPath)[0] === '_') {
 		return;
 	}
 
-	var fullInputPath = compileStep._fullInputPath;
-	var rootDir = findRootDir(fullInputPath);
+	var rootDir = findRootDir(compileStep._fullInputPath);
+
+	var f = new Future;
 
 	// OPTIONS ========================================
 
 	var optionsPath = path.join(rootDir, OPTIONS_FILENAME);
 	var options = readJSON(optionsPath, compileStep);
-	options = _.extend(options, {
-		sourceComments: 'none',
-		includePaths: [],
-		stats: {}
+	_.defaults(options, {
+		//sourceComments: 'map',
+		//sourceMap: '',
+		includePaths: [
+			'tests/partials'
+		]
 	});
-	options.file = fullInputPath; // not in extend because this shouldn't be found in the options json
-	options.includePaths.push(path.dirname(fullInputPath));
+	options.file = compileStep.inputPath;
+	options.success = function(css, sourceMap) {
+		var result = {
+			css: css,
+			sourceMap: sourceMap
+		};
+		f.return(result)
+	};
+	// parses the error string
+	options.error = function(err) {
+		var re = /^(.*\.(?:scss|sass)):([0-9]+?): (.*)$/;
+		var match = err.trim().match(re);
+		var message = 'Sass ' + match[3];
+		err = new Error(message);
+		err.sourcePath = match[1];
+		err.lineNumber = match[2];
+		f.return(err);
+	};
+	options.stats = {};
+	//console.log('options', options.includePaths);
 
 	// COMPILE ========================================
 
-	var css;
-	try {
-		css = sass.renderSync(options);
-	} catch (err) {
-		err = err.replace(fullInputPath + ':', '').split(':');
-		var errMessage = err[1].trim() + ': ' + err[2].trim(),
-			errLine = err[0];
+	sass.render(options);
+	var result = f.wait();
+	if (result instanceof Error) {
 		compileStep.error({
-			message: 'Sass compiler ' + errMessage,
-			sourcePath: compileStep.inputPath,
-			line: errLine
+			message: result.message,
+			sourcePath: result.sourcePath,
+			line: result.lineNumber
 		});
-		return;
+	} else {
+		// TODO add result.sourceMap when node-sass segmentation fault fix is released
+		// possible other way of getting sourceMaps with renderSync -> options.stats.sourceMap?
+		console.log(result.css);
+		compileStep.addStylesheet({
+			path: compileStep.inputPath + '.css',
+			data: result.css
+		});
+	}
+});
+
+/**
+ * sass_include_paths.json SHOULD BE PUT IN PACKAGES ONLY EITHER FOR TESTING OR FOR A SASS LIBRARY
+ *
+ * - When adding sass_include_paths.json to a package, it must be added before any sass/scss.
+ * - Paths in sass_include_paths.json should be relative to sass_include_paths.json
+ * - sass_options.json is only stored in packages for testing purposes
+ *
+ * 1. Merge `includePaths` from each 'sass_include_paths.json' added using `api.addFiles` to 'sass_options.json'
+ * 2. Store 'sass_options.json' in the app root dir
+ */
+var INCLUDE_PATHS_FILENAME = 'sass_include_paths.json';
+Plugin.registerSourceHandler(INCLUDE_PATHS_FILENAME, function(compileStep) {
+	var rootDir = findRootDir(compileStep._fullInputPath);
+
+	// full path of the directory sass_include_paths.json is in
+	var relativeDir = path.dirname(compileStep._fullInputPath);
+	console.log(relativeDir);
+
+	// OPTIONS ========================================
+
+	var optionsPath = path.join(rootDir, OPTIONS_FILENAME);
+	var options = readJSON(optionsPath, compileStep);
+	// because the options file might not exist yet
+	if (!_.has(options, 'includePaths')) {
+		options.includePaths = [];
 	}
 
-	compileStep.addStylesheet({
-		path: compileStep.inputPath + ".css",
-		data: css
+	// the includePaths loaded from sass_include_paths.json
+	var includePaths = readJSON(compileStep.inputPath);
+	includePaths = _.map(includePaths, function(includePath) {
+		return path.relative(rootDir, path.join(relativeDir, includePath));
 	});
+
+	options.includePaths = _.union(options.includePaths, includePaths);
+
+	// WRITE ========================================
+
+	// files.addToGitignore(rootDir, OPTIONS_FILENAME);
+
+	writeJSON(optionsPath, options, compileStep);
 });

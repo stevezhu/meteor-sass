14ce10ff67277e315ca71be30e01f5172eed5e98
Steve Zhu <st.zhu1@gmail.com>
2015-03-19 12:24:19 -0400

    Source maps

--- plugin/compile-sass.js
+++ plugin/compile-sass.js
@@ -9,5 +9,35 @@ Plugin.registerSourceHandlers = function(extensions, options, handler) {
 };
 
 Plugin.registerSourceHandlers(['sass', 'scss'], {archMatching: 'web'}, function(compileStep) {
-	
+	var source = compileStep.read().toString('utf8');
+	console.log("Compiling file:", compileStep.inputPath);
+	var future = new Future();
+
+	try {
+		sass.render({
+			file: compileStep.fullInputPath,
+			sourceMap: true,
+			omitSourceMapUrl: true,
+			outFile: "file.css",
+			success: function(result) {
+				future.return(result);
+			},
+			error: function() {
+				future.throw("wot error");
+			}
+		});
+
+		var result = future.wait();
+		console.log("Result:", result);
+
+		if (result.map) {
+			var sourceMap = JSON.parse(result.map);
+			delete sourceMap.file;
+			sourceMap.sources = [compileStep.inputPath];
+			sourceMap.sourcesContent = [source];
+			console.log("Source map:", sourceMap);
+		}
+	} catch (e) {
+		console.log(e);
+	}
 });
